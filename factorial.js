38fccebe9402c64ec486825fe1245fd3ffcbd2ba
sahil khan <sahilkhan2002786@gmail.com>
2024-02-04 16:36:31 +0530

    Binomial coefficient

--- factorial.js
+++ factorial.js
@@ -1,9 +1,21 @@
 
-const factorial =()=>{
+const factorial =(n)=>{
     let f=1;
-    for(i =1; i<=6; i++){
+    for(i =1; i<=n; i++){
         f*=i;
     }
     return f;
 }
+function binomial(a, b){
+    let facta = factorial(a);
+    let factb= factorial(b);
+
+    let finalBino =factorial(a-b);
+
+    let binom= facta / (factb*finalBino)
+    return binom
+
+
+}
+console.log(binomial(5,3))
 console.log(factorial(5))

a1bff41deccd61dc549c313178fd06dfa81d7a0c
sahil khan <sahilkhan2002786@gmail.com>
2024-02-08 22:06:06 +0530

    spread operstor

--- index2.html/index.js
+++ index2.html/index.js
@@ -3,3 +3,14 @@ const obj ={
     age: 25,
     address: 'delhi'
 }
+
+// spread operator
+const originalArray = [1, 2, 3];
+const copyArray = [...originalArray];
+console.log(copyArray); // Output: [1, 2, 3]
+
+// Concatenating arrays
+const array1 = [1, 2, 3];
+const array2 = [4, 5, 6];
+const concatenatedArray = [...array1, ...array2];
+console.log(concatenatedArray); // Output: [1, 2, 3, 4, 5, 6]

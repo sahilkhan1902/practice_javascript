43b2513cfd183861afde91cb129e5410eed3235d
sahil khan <sahilkhan2002786@gmail.com>
2024-02-11 20:58:33 +0530

    rest operator

--- index2.html/index.js
+++ index2.html/index.js
@@ -14,3 +14,13 @@ const array1 = [1, 2, 3];
 const array2 = [4, 5, 6];
 const concatenatedArray = [...array1, ...array2];
 console.log(concatenatedArray); // Output: [1, 2, 3, 4, 5, 6]
+
+
+// Function that sums all its arguments
+function sum(...numbers) {
+    return numbers.reduce((acc, val) => acc + val, 0);
+}
+
+console.log(sum(1, 2, 3)); // Output: 6
+console.log(sum(1, 2, 3, 4, 5)); // Output: 15
+

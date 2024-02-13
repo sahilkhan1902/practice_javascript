0fc01f3acf02dc48a57a6f50b1e69bc486b9a302
sahil khan <sahilkhan2002786@gmail.com>
2024-02-13 22:03:09 +0530

    Hoisting

--- practise.js
+++ practise.js
@@ -1,8 +1,10 @@
+//Hoisting:- When you try to access the variable prior it initialization 
+// only var can be hoister 
+// let and const can't be hoisted they just go through temporal deadzone where it hold undefined at the time of memory allocation
 console.log(x); // undefined
 var x = 5;
 console.log(x); // 5
 
-// The above code is equivalent to:
 var x; // Declaration is hoisted to the top
 console.log(x); // undefined
 x = 5; // Assignment remains in place

2f0d260a65b2cb044633e4473442ea9c1245d684
sahil khan <sahilkhan2002786@gmail.com>
2024-02-15 23:08:24 +0530

    DectoBin

--- dectobin.js
+++ dectobin.js
@@ -1,6 +1,6 @@
-let power =0;
-let bin =0;
 function dectobin(n){
+    let power =0;
+    let bin =0;
 while (n>0){
     let rem = n%2
 bin = bin + (rem* Math.pow(10, power));
@@ -9,4 +9,4 @@ n=Math.floor(n/2)
 }
 return bin
 }
-console.log(dectobin(101))
+console.log(dectobin(7))
